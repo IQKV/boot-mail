2443f47ceb459cc94d12d4eec69f08083767b779
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-07-19 20:01:40 +0300

    feat: init mail support properties
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/main/java/org/iqkv/boot/mail/MailProperties.java
@@ -0,0 +1,34 @@
+package org.iqkv.boot.mail;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "iqkv.mail", ignoreUnknownFields = false)
+public class MailProperties {
+  private boolean enabled = false;
+  private String from = "";
+  private String baseUrl = "";
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public String getBaseUrl() {
+    return baseUrl;
+  }
+
+  public void setBaseUrl(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+}
